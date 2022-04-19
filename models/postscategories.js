022e3f8efe2ee72bf4fd1feacb450db7010fa62c
Renan Souza <s.renan@outlook.com>
2022-04-19 15:16:45 -0300

    fix: erro no nome da tabela na model PostCategory

--- models/postscategories.js
+++ models/postscategories.js
@@ -2,7 +2,10 @@ module.exports = (sequelize, DataTypes) => {
   const PostCategory = sequelize.define('PostCategory', {
     postId: DataTypes.INTEGER,
     categoryId: DataTypes.INTEGER,
-  }, { timestamps: false });
+  }, {
+    timestamps: false,
+    tableName: 'PostsCategories',
+  });
 
   PostCategory.associate = (models) => {
     models.BlogPost.belongsToMany(
